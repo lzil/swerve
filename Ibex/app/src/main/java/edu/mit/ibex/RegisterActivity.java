53999d7b1daf036f27ab5874b0db646b7845d61c
Liang <zhoul@mit.edu>
2015-04-21 23:25:11 -0400

    ui mockup

--- Ibex/app/src/main/java/edu/mit/ibex/RegisterActivity.java
+++ Ibex/app/src/main/java/edu/mit/ibex/RegisterActivity.java
@@ -77,6 +77,7 @@ public class RegisterActivity extends ActionBarActivity {
         //if pass:
         myFirebase.child(usr + "/status").setValue("");
         myFirebase.child(usr + "/password").setValue(psw);
+        myFirebase.child(usr + "/friends").setValue("");
         Intent intent = new Intent(this, StatusActivity.class);
         intent.putExtra("username", usr);
         startActivity(intent);
